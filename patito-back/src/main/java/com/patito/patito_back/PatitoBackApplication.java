76f21f061aff163b92216c435ff6f473d269fd97
Gab0CS <gabocanseco@gmail.com>
2026-06-24 16:24:43 -0600

    Back-end agregado

---
+++ patito-back/src/main/java/com/patito/patito_back/PatitoBackApplication.java
@@ -0,0 +1,13 @@
+package com.patito.patito_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitoBackApplication.class, args);
+	}
+
+}
