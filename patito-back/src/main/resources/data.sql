93d40085275860a223d3e7d47db01287c10c980e
Gab0CS <gabocanseco@gmail.com>
2026-06-25 12:55:22 -0600

    Dummy data para test

---
+++ patito-back/src/main/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO productos (
+    hawa,
+    nombre,
+    precio_lista,
+    descuento,
+    existencias
+)
+VALUES
+('HAWA001', 'Ford Ranger', 850000.00, 15000.00, 5);
+
+INSERT INTO productos (
+    hawa,
+    nombre,
+    precio_lista,
+    descuento,
+    existencias
+)
+VALUES
+('HAWA002', 'Toyota Hilux', 920000.00, 10000.00, 10);
+
+INSERT INTO productos (
+    hawa,
+    nombre,
+    precio_lista,
+    descuento,
+    existencias
+)
+VALUES
+('HAWA003', 'Nissan Frontier', 890000.00, 0.00, 3);
